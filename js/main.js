ec5fe5fdc3217cc78178dee15c23f80bb5d56e7f
CarlosLopez98 <carlos980718@gmail.com>
2020-05-04 11:57:44 -0500

    Estilos css

--- js/main.js
+++ js/main.js
@@ -4,6 +4,6 @@ function calcular_tiempo_descanso(){
   t_ejercicio = document.getElementById('t_actividad').value
   if(t_ejercicio >= 20 && t_ejercicio <= 60){
     t_descanso = 60 - parseInt(t_ejercicio)
-    document.getElementById('t_descanso').innerHTML = "Tiempo de descanso: " + t_descanso
+    document.getElementById('t_descanso').innerHTML = "Tiempo de descanso: " + t_descanso + " segundos"
   }
 }
