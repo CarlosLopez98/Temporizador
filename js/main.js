68c9d06ea37f80cc2bba3a60d846a53958b88657
CarlosLopez98 <carlos980718@gmail.com>
2020-05-04 11:32:15 -0500

    Función que calcula el tiempo de descanso

---
+++ js/main.js
@@ -0,0 +1,9 @@
+window.onload = calcular_tiempo_descanso
+
+function calcular_tiempo_descanso(){
+  t_ejercicio = document.getElementById('t_actividad').value
+  if(t_ejercicio >= 20 && t_ejercicio <= 60){
+    t_descanso = 60 - parseInt(t_ejercicio)
+    document.getElementById('t_descanso').innerHTML = "Tiempo de descanso: " + t_descanso
+  }
+}
